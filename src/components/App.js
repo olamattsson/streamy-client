b1c9d137cce81e3a574604a9f10c194af76384b9
Ola Mattsson <ola.mattsson@gmail.com>
2020-05-28 17:03:46 +0200

    #211 Remove dummy components and routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import {BrowserRouter, Link, Route} from "react-router-dom";
 
-const PageOne = () => {
-    return (
-        <div>
-            PageOne<br/>
-            <Link to={"/pagetwo"}>Go to PageTwo</Link>
-        </div>
-    );
-}
-const PageTwo = () => {
-    return (
-        <div>
-            PageTwo<br/>
-            <Link to={"/"}>Go to PageOne</Link>
-        </div>
-    );
-}
 const App = () => {
     return (
         <div>
             <BrowserRouter>
-                <Route path={"/"} exact component={PageOne}/>
-                <Route path={"/pagetwo"} component={PageTwo}/>
             </BrowserRouter>
         </div>
     );
