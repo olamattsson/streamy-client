2866863b7671bfc9a95dd395715d663a2744a2ff
Ola Mattsson <ola.mattsson@gmail.com>
2020-05-28 17:33:29 +0200

    #213 Add Header component to screen

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,12 @@ import StreamDelete from "./streams/StreamDelete"
 import StreamEdit from "./streams/StreamEdit"
 import StreamList from "./streams/StreamList"
 import StreamShow from "./streams/StreamShow"
+import Header from "./Header";
 
 const App = () => {
     return (
         <div>
+            <Header/>
             <BrowserRouter>
                 <Route path={"/"} exact component={StreamList}/>
                 <Route path={"/streams/new"} exact component={StreamCreate}/>
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+const Header = (props) => {
+    return (
+        <div>
+            Header
+        </div>
+    );
+};
+export default Header;
