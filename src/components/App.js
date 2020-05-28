1dd58910cb8063dbbbdac89ac006c9078af614eb
Ola Mattsson <ola.mattsson@gmail.com>
2020-05-28 17:19:42 +0200

    #211 Add routes to stream components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {BrowserRouter, Link, Route} from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate"
+import StreamDelete from "./streams/StreamDelete"
+import StreamEdit from "./streams/StreamEdit"
+import StreamList from "./streams/StreamList"
+import StreamShow from "./streams/StreamShow"
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
+                <Route path={"/"} exact component={StreamList}/>
+                <Route path={"/streams/new"} exact component={StreamCreate}/>
+                <Route path={"/streams/edit"} exact component={StreamEdit}/>
+                <Route path={"/streams/delete"} exact component={StreamDelete}/>
+                <Route path={"/streams/show"} exact component={StreamShow}/>
             </BrowserRouter>
         </div>
     );
