3718e7af65ddee9bf0bd2fde5f1c93101396a7bb
Ola Mattsson <ola.mattsson@gmail.com>
2020-05-28 14:55:17 +0200

    #204 temp code to show example of router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
 
+const PageOne = () => {
+    return <div>PageOne</div>
+}
+const PageTwo = () => {
+    return <div>PageTwo</div>
+}
 const App = () => {
     return (
         <div>
-            App
+            <BrowserRouter>
+                <Route path={"/"} exact component={PageOne} />
+                <Route path={"/pagetwo"} component={PageTwo} />
+            </BrowserRouter>
         </div>
     );
 }
