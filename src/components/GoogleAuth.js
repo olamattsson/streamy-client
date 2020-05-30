21473c2d0990cd7b2388c751e47738ad71c826f6
Ola Mattsson <ola.mattsson@gmail.com>
2020-05-30 14:59:26 +0200

    #222 Change ui to show button with appropriate text

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -22,11 +22,21 @@ class GoogleAuth extends React.Component {
 
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
-            return <div>I don't know if we are signed in</div>
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>We are signed in</div>
+            return (
+                <button className={"ui red google button"}  >
+                    <i className={"google icon"}/>
+                    Sign Out
+                </button>
+            )
         }
-        return <div>We are not signed in</div>
+        return (
+            <button className={"ui red google button"}  >
+                <i className={"google icon"}/>
+                Sign In with Google
+            </button>
+        )
     }
 
     render() {
