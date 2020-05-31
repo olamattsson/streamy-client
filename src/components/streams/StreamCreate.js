5167cc3af78f686494d8ddae9b08cd2aafd42061
Ola Mattsson <ola.mattsson@gmail.com>
2020-05-31 17:09:53 +0200

    #236 Start using redux-form in StreamCreate component, not ready for use just yet.

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import {Field, reduxForm} from "redux-form";
 
-const StreamCreate = (props) => {
-    return (
-        <div>
-            StreamCreate
-        </div>
-    );
-};
-export default StreamCreate;
+class StreamCreate extends React.Component {
+    render() {
+        return (
+            <form>
+                <Field name={"title"} />
+                <Field name={"description"} />
+            </form>
+
+        );
+    };
+}
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
