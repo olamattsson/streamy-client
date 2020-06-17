10be23a463491a380cacb5c0251f9d9133fbb234
Ola Mattsson <ola.mattsson@gmail.com>
2020-06-17 18:00:56 +0200

    #259 Compare userId so current user can edit his own streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -6,10 +6,28 @@ class StreamList extends React.Component {
     componentDidMount() {
         this.props.fetchStreams();
     }
+
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className={"right floated content"}>
+                    <button className={"ui button primary"}>
+                        Edit
+                    </button>
+                    <button className={"ui button negative"}>
+                        Delete
+                    </button>
+                </div>
+            );
+        }
+        return null;
+    }
+
     renderList() {
         return this.props.streams.map(stream => {
             return (
                 <div className={"item"} key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className={"large middle aligned camera icon"}/>
                     <div className={"content"}>
                         {stream.title}
@@ -21,6 +39,7 @@ class StreamList extends React.Component {
             );
         });
     }
+
     render() {
         return (
             <div>
@@ -34,6 +53,9 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-    return {streams: Object.values(state.streams)}
+    return {
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId
+    }
 }
 export default connect(mapStateToProps, {fetchStreams})(StreamList);
