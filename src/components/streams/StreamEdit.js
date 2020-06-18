5b2124b79c194b80ccdc82eae81eb0201ee0af16
Ola Mattsson <ola.mattsson@gmail.com>
2020-06-18 16:09:48 +0200

    #270 Try to fetch wanted stream from state, only works when user click button on list page

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 
 const StreamEdit = (props) => {
     return (
@@ -7,4 +8,8 @@ const StreamEdit = (props) => {
         </div>
     );
 };
-export default StreamEdit;
+
+const mapStateToProps = (state, ownProps) => {
+    return {stream: state.streams[ownProps.match.params.id]};
+}
+export default connect(mapStateToProps)(StreamEdit);
