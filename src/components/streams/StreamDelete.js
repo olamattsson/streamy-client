9cd66ce2d7f53e65f6a1563894dad8b95253bf79
Ola Mattsson <ola.mattsson@gmail.com>
2020-06-19 00:48:20 +0200

    #285 Define dismiss behavior of modal from parent component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Modal from "../Modal";
+import history from "../../history";
 const StreamDelete = (props) => {
     const actions = (
         <React.Fragment>
@@ -14,6 +15,7 @@ const StreamDelete = (props) => {
                 title={"Delete Stream"}
                 content={"Are you sure you want to delete this stream?"}
                 actions={actions}
+                onDismiss={()=> history.push('/')}
             />
         </div>
     );
