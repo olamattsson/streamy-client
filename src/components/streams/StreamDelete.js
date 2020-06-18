02f5d1adeabab18d172fd2e63c61f50a11928e18
Ola Mattsson <ola.mattsson@gmail.com>
2020-06-19 00:32:11 +0200

    #283 Configure content of Modal display from parent component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import Modal from "../Modal";
 const StreamDelete = (props) => {
+    const actions = (
+        <div>
+            <button className={"ui button negative"}>Delete</button>
+            <button className={"ui button"}>Cancel</button>
+        </div>
+    );
     return (
         <div>
             StreamDelete
-            <Modal/>
+            <Modal
+                title={"Delete Stream"}
+                content={"Are you sure you want to delete this stream?"}
+                actions={actions}
+            />
         </div>
     );
 };
