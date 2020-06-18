df7bc4e96e2d3431dcf977dac173f836d191fc60
Ola Mattsson <ola.mattsson@gmail.com>
2020-06-18 20:00:12 +0200

    #275 Edit stream and reuse form code

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {fetchStream} from '../../actions';
+import {fetchStream, editStream} from '../../actions';
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
+    }
+
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.stream.id, formValues);
     }
 
     render() {
@@ -13,7 +18,11 @@ class StreamEdit extends React.Component {
         }
         return (
             <div>
-                {this.props.stream.title}
+                <h3>Edit a Stream</h3>
+                <StreamForm
+                    initialValues={this.props.stream}
+                    onSubmit={this.onSubmit}
+                />
             </div>
         );
     }
@@ -22,4 +31,4 @@ class StreamEdit extends React.Component {
 const mapStateToProps = (state, ownProps) => {
     return {stream: state.streams[ownProps.match.params.id]};
 }
-export default connect(mapStateToProps, {fetchStream})(StreamEdit);
+export default connect(mapStateToProps, {fetchStream, editStream})(StreamEdit);
