367d95ee45badfafc32e50703527762bd43a9689
Ola Mattsson <ola.mattsson@gmail.com>
2020-06-18 23:22:51 +0200

    #278 Use Patch instead of Put to only update part of the object instead of replacing it completely

--- src/actions/index.js
+++ src/actions/index.js
@@ -33,7 +33,7 @@ export const fetchStream = (id) => async (dispatch) => {
     dispatch({ type: FETCH_STREAM, payload: response.data});
 }
 export const editStream = (id, formValues) => async (dispatch) => {
-    const response = await streams.put(`/streams/${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
 
     dispatch({ type: EDIT_STREAM, payload: response.data});
     createBrowserHistory.push("/");
