5fea9564212e25d5032a1930fcff156cdae0976e
Ola Mattsson <ola.mattsson@gmail.com>
2020-06-19 16:19:44 +0200

    ##286 Show title and description of selected stream on the StreamShow component

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,27 @@
-import * as React from 'react';
+import React from "react";
+import {connect} from "react-redux";
+import {fetchStream} from "../../actions";
 
-const StreamShow = (props) => {
-    return (
-        <div>
-            StreamShow
-        </div>
-    );
-};
-export default StreamShow;
+class StreamShow extends React.Component{
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return (<div>Loading...</div>);
+        }
+        const {title, description} = this.props.stream;
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        )
+    }
+
+}
+const mapStateToProps = (state, ownProps) => {
+    return {stream:state.streams[ownProps.match.params.id]}
+}
+export default connect(mapStateToProps, {fetchStream})(StreamShow);
