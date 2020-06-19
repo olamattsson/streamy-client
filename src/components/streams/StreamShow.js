c3a7a68875b13a87fe95c6fa616c58b96977cea6
Ola Mattsson <ola.mattsson@gmail.com>
2020-06-19 16:37:07 +0200

    ##292 Set up video player with DOM reference, 100% width and controls set to true

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -20,6 +20,7 @@ class StreamShow extends React.Component{
         const {title, description} = this.props.stream;
         return (
             <div>
+                <video ref={this.videoRef} style={{ width:'100%' }} controls />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
