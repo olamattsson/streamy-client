86340b9b0fdbe3a6bdef9bc89aee326a617e96cb
Ola Mattsson <ola.mattsson@gmail.com>
2020-06-19 14:57:36 +0200

    ##285 Use Switch tag to select exactly one "route" to show instead of all matching routes.
    Before the change the /streams/new path matched the /streams/:id path too so both components were shown.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router, Route} from "react-router-dom";
+import {Route, Router, Switch} from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate"
 import StreamDelete from "./streams/StreamDelete"
 import StreamEdit from "./streams/StreamEdit"
@@ -7,16 +7,19 @@ import StreamList from "./streams/StreamList"
 import StreamShow from "./streams/StreamShow"
 import Header from "./Header";
 import createBrowserHistory from "../history"
+
 const App = () => {
     return (
         <div className={"ui container"}>
             <Router history={createBrowserHistory}>
                 <Header/>
-                <Route path={"/"} exact component={StreamList}/>
-                <Route path={"/streams/new"} exact component={StreamCreate}/>
-                <Route path={"/streams/edit/:id"} exact component={StreamEdit}/>
-                <Route path={"/streams/delete/:id"} exact component={StreamDelete}/>
-                <Route path={"/streams/:id"} exact component={StreamShow}/>
+                <Switch>
+                    <Route path={"/"} exact component={StreamList}/>
+                    <Route path={"/streams/new"} exact component={StreamCreate}/>
+                    <Route path={"/streams/edit/:id"} exact component={StreamEdit}/>
+                    <Route path={"/streams/delete/:id"} exact component={StreamDelete}/>
+                    <Route path={"/streams/:id"} exact component={StreamShow}/>
+                </Switch>
             </Router>
         </div>
     );
